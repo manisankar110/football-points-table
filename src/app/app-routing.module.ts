c1d691b5ea6d1ec74c5f31d7f6f9a3b024def77f
sankarmani110@gmail.com <sankarmani110@gmail.com>
2023-12-08 16:07:27 +0530

    latest-build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent,
+    component: AppComponent,
   },
   {
     path: ':league',
